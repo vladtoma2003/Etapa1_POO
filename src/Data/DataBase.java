7fdb9b7f0bb832c11e7760a54e81c2cbaa04b080
Vlad Iliescu <vladtoma.iliescu@gmail.com>
2022-12-13 21:46:14 +0200

    added Lombok, the cool guy

--- src/Data/DataBase.java
+++ src/Data/DataBase.java
@@ -1,24 +1,18 @@
 package Data;
 
-import Factory.MovieFactory;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.ArrayList;
 
+@Setter
+@Getter
 public class DataBase {
     private ArrayList<User> users = new ArrayList<>();
     private ArrayList<Movie> availableMovies = new ArrayList<>();
     private ArrayList<Movie> currentMoviesList = new ArrayList<>();
     private User loggedUser = null;
     private String currentMovie = null;
-
-
-    public String getCurrentMovie() {
-        return currentMovie;
-    }
-
-    public void setCurrentMovie(String currentMovie) {
-        this.currentMovie = currentMovie;
-    }
 
     public boolean existsUser(User user) {
         return users.stream()
@@ -40,25 +34,15 @@ public class DataBase {
     public Movie getMovieFromCurrentList(String name) {
         for (var movie : currentMoviesList) {
             if (movie.getName().startsWith(name)) {
-//                return MovieFactory.newMovie(movie);
                 return movie;
             }
         }
         return null;
     }
-//    public Movie getRefrenceCurrentList(String name) {
-//        for (var movie : currentMoviesList) {
-//            if (movie.getName().startsWith(name)) {
-//                return movie;
-//            }
-//        }
-//        return null;
-//    }
 
     public Movie getPurchasedMovies(String name) {
         for (var movie : loggedUser.getPurchasedMovies()) {
             if (movie.getName().startsWith(name)) {
-//                return MovieFactory.newMovie(movie);
                 return movie;
             }
         }
@@ -68,48 +52,9 @@ public class DataBase {
     public Movie getWatchedMovies(String name) {
         for (var movie : loggedUser.getWatchedMovies()) {
             if (movie.getName().startsWith(name)) {
-//                return MovieFactory.newMovie(movie);
                 return movie;
             }
         }
         return null;
-    }
-
-
-    public boolean existsMovie(Movie movie) {
-        return currentMoviesList.stream()
-                .anyMatch(o -> o.equals(movie));
-    }
-
-    public ArrayList<User> getUsers() {
-        return users;
-    }
-
-    public void setUsers(ArrayList<User> users) {
-        this.users = users;
-    }
-
-    public ArrayList<Movie> getAvailableMovies() {
-        return availableMovies;
-    }
-
-    public void setAvailableMovies(ArrayList<Movie> availableMovies) {
-        this.availableMovies = availableMovies;
-    }
-
-    public ArrayList<Movie> getCurrentMoviesList() {
-        return currentMoviesList;
-    }
-
-    public void setCurrentMoviesList(ArrayList<Movie> currentMoviesList) {
-        this.currentMoviesList = currentMoviesList;
-    }
-
-    public User getLoggedUser() {
-        return loggedUser;
-    }
-
-    public void setLoggedUser(User loggedUser) {
-        this.loggedUser = loggedUser;
     }
 }
